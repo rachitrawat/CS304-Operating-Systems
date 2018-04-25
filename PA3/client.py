211613f5f19ab106cf0a2c68f8473ea3e4d9bc1d
rachitrawat <rachit.rc.rawat@gmail.com>
2018-04-25 12:19:53 +0530

    sync

---
+++ PA3/client.py
@@ -0,0 +1,24 @@
+import inotify.adapters
+
+# Only log events the following events:
+# file moved to/from
+# file modified
+log_events_list = [['IN_MOVED_TO'], ['IN_CLOSE_WRITE'], ['IN_MOVED_FROM']]
+
+
+def _main():
+    i = inotify.adapters.Inotify()
+
+    i.add_watch('/home/su/PycharmProjects/Operating-Systems/PA3/watch_folder')
+
+    for event in i.event_gen(yield_nones=False):
+        (_, type_names, path, filename) = event
+
+        # ignore filename = .goutputstream* ['IN_CLOSE_WRITE'] is sufficient
+        if filename != '' and ".goutputstream" not in filename and type_names in log_events_list:
+            print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
+                path, filename, type_names))
+
+
+if __name__ == '__main__':
+    _main()
